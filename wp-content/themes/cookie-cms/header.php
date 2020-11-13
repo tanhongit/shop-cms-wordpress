973d6e5be489f43ad9058e60243312d848c030ac
tanhongit <phuongtan12357nguyen@gmail.com>
2020-11-14 02:03:08 +0700

    create a theme structure and write functions.php file

---
+++ wp-content/themes/cookie-cms/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html <?php language_attributes(); ?> class="ie8"> <![endif]-->
+<!--[if !IE]> <html <?php language_attributes(); ?>> <![endif]-->
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <link rel="profile" href="http://gmgp.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+    <?php wp_head(); ?> <!--hook này để giúp WordPress hiểu được đây là khu vực thẻ <head>-->
+</head>
+
+<body <?php body_class(); ?>>
+    <!--Thêm class tượng trưng cho mỗi trang lên <body> để tùy biến-->
+    <div id="container">
