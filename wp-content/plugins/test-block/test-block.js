aeb45c4311ef59b1e82bb723695e2d20e8dd6c45
Thuận Trần <thuankg1752@gmail.com>
2020-12-05 00:39:05 +0700

    Add regex cho email box

--- wp-content/plugins/test-block/test-block.js
+++ wp-content/plugins/test-block/test-block.js
@@ -1,5 +1,8 @@
+/* This section of the code registers a new block, sets an icon
+and a category, and indicates what type of fields it'll include.
+*/
 wp.blocks.registerBlockType("brad/border-box", {
-  title: "Simple Box",
+  title: "Email Box",
   icon: "smiley",
   category: "common",
   attributes: {
@@ -10,14 +13,24 @@ wp.blocks.registerBlockType("brad/border-box", {
   edit: function (props) {
     function updateContent(event) {
       props.setAttributes({ content: event.target.value });
+      // biểu thức chính quy
+      let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+      var emailInput = event.target.value;
+      var errorEmail = document.getElementById("email");
+      if (reg.test(emailInput) != true) {
+        errorEmail.style.border = "3px solid red";
+      } else {
+        errorEmail.style.border = "3px solid black";
+      }
     }
     return React.createElement(
       "div",
       null,
-      React.createElement("h3", null, "Simple Box"),
+      React.createElement("h3", null, "Email Box"),
       React.createElement("input", {
         type: "text",
         value: props.attributes.content,
+        id: "email",
         onChange: updateContent,
       })
     );
@@ -25,7 +38,7 @@ wp.blocks.registerBlockType("brad/border-box", {
   save: function (props) {
     return wp.element.createElement(
       "h3",
-      { style: { border: "3px solid " + props.attributes.color } },
+      { style: { border: "3px solid black" } },
       props.attributes.content
     );
   },
