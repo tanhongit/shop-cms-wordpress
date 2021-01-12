4b20b84264d7275569478d5a91d3e488020b675d
tanhongit <phuongtan12357nguyen@gmail.com>
2021-01-12 10:22:11 +0700

    update homepage

--- wp-content/themes/cookie-cms/index.php
+++ wp-content/themes/cookie-cms/index.php
@@ -3,26 +3,8 @@
 <!-- body -->
 <!-- Header Area End -->
 
-<!-- Hero Area Start -->
-<!-- Hero Area End -->
-
 <!-- Show message if product added to cart-->
 <?php do_action('woocommerce_before_cart'); ?>
-
-<!-- Banner Four Area Start -->
-<!-- Banner Four Area End -->
-
-<!-- Protuct Area Start -->
-<?php
-if (have_posts()) :
-    while (have_posts()) : the_post();
-?>
-        <?php the_content() ?>
-<?php endwhile;
-else :
-    echo "<p>khong co post nao het</p>";
-endif; ?>
-<!-- Protuct Area End -->
 
 <!-- footer -->
 <?php get_footer(); ?>
