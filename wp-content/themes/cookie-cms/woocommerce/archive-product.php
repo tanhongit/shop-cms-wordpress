b70da9913f64224a248f24f6e882ae203cb35f13
tanhongit <phuongtan12357nguyen@gmail.com>
2021-01-15 04:17:45 +0700

    woo plugin update

--- wp-content/themes/cookie-cms/woocommerce/archive-product.php
+++ wp-content/themes/cookie-cms/woocommerce/archive-product.php
@@ -32,7 +32,7 @@ do_action('woocommerce_before_main_content');
 ?>
 <header class="woocommerce-products-header" style="text-align: center;">
 	<?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
-		<h1 class="woocommerce-products-header__title page-title">The Category: <?php woocommerce_page_title(); ?></h1>
+		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 	<?php endif; ?>
 
 	<?php
