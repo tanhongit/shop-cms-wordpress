46e6e33db046758395413884602dfa86f0902d0e
tanhongit <phuongtan12357nguyen@gmail.com>
2021-02-16 12:00:44 +0700

    update form edit account

--- wp-content/themes/cookie-cms/woocommerce/myaccount/form-edit-account.php
+++ wp-content/themes/cookie-cms/woocommerce/myaccount/form-edit-account.php
@@ -17,7 +17,7 @@
  */
 
 defined('ABSPATH') || exit;
-
+$current_user = wp_get_current_user();
 do_action('woocommerce_before_edit_account_form'); ?>
 <div class="tab-pane fade" id="account-info" role="tabpanel">
 	<div class="myaccount-content">
@@ -29,24 +29,24 @@ do_action('woocommerce_before_edit_account_form'); ?>
 					<div class="col-lg-6">
 						<div class="single-input-item">
 							<label for="account_first_name" class="required"><?php esc_html_e('First name', 'woocommerce'); ?></label>
-							<input type="text" name="account_first_name" id="account_first_name" autocomplete="given-name" value="<?php echo esc_attr($user->first_name); ?>" />
+							<input type="text" name="account_first_name" value="<?= esc_html($current_user->user_firstname) ?>" id="account_first_name" autocomplete="given-name" value="<?php echo esc_attr($user->first_name); ?>" />
 						</div>
 					</div>
 					<div class="col-lg-6">
 						<div class="single-input-item">
 							<label for="account_last_name"><?php esc_html_e('Last name', 'woocommerce'); ?></label>
-							<input type="text" name="account_last_name" id="account_last_name" autocomplete="family-name" value="<?php echo esc_attr($user->last_name); ?>" />
+							<input type="text" name="account_last_name" value="<?= esc_html($current_user->user_lastname) ?>" id="account_last_name" autocomplete="family-name" value="<?php echo esc_attr($user->last_name); ?>" />
 						</div>
 					</div>
 				</div>
 				<div class="single-input-item">
 					<label for="account_display_name" class="required"><?php esc_html_e('Display name', 'woocommerce'); ?></label>
-					<input type="text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr($user->display_name); ?>" />
+					<input type="text" name="account_display_name" value="<?= esc_html($current_user->display_name) ?>" id="account_display_name" value="<?php echo esc_attr($user->display_name); ?>" />
 					<span><em><?php esc_html_e('This will be how your name will be displayed in the account section and in reviews', 'woocommerce'); ?></em></span>
 				</div>
 				<div class="single-input-item">
 					<label for="account_email"><?php esc_html_e('Email address', 'woocommerce'); ?>&nbsp;<span class="required">*</span></label>
-					<input type="email" name="account_email" id="account_email" autocomplete="email" value="<?php echo esc_attr($user->user_email); ?>" />
+					<input type="email" name="account_email" id="account_email" autocomplete="email" value="<?= esc_html($current_user->user_email) ?>" />
 				</div>
 
 
