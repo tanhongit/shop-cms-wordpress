32437f814d006f7a127d987c432ecedfa0fb9c2d
tanhongit <phuongtan12357nguyen@gmail.com>
2021-02-22 13:11:02 +0700

    Update cart: COUPON

--- wp-content/themes/cookie-cms/woocommerce/cart/cart-totals.php
+++ wp-content/themes/cookie-cms/woocommerce/cart/cart-totals.php
@@ -25,22 +25,72 @@ defined('ABSPATH') || exit;
 			<?php do_action('woocommerce_before_cart_totals'); ?>
 			<h2><?php esc_html_e('Cart totals', 'woocommerce'); ?></h2>
 			<div class="table-total-amount">
+
 				<div class="single-total-content d-flex justify-content-between">
 					<span><?php esc_html_e('Subtotal', 'woocommerce'); ?></span>
 					<span class="c-total-price"><?php wc_cart_totals_subtotal_html(); ?></span>
 				</div>
-				<!-- <div class="single-total-content d-flex justify-content-between">
-                        <span>Shipping</span>
-                        <span class="c-total-price"><span>Flat Rate:</span> $0.00</span>
-                    </div>
-                    <div class="single-total-content d-flex justify-content-end">
-                        <a href="#">Calculate shipping</a>
-					</div> -->
+				<hr>
+
+				<?php foreach (WC()->cart->get_coupons() as $code => $coupon) : ?>
+					<div class="single-total-content d-flex justify-content-between">
+						<span><?php wc_cart_totals_coupon_label($coupon); ?></span>
+						<span class="c-total-price"><?php wc_cart_totals_coupon_html($coupon); ?></span>
+					</div>
+				<?php endforeach; ?>
+				<hr>
+
+				<?php if (WC()->cart->needs_shipping() && WC()->cart->show_shipping()) : ?>
+					<?php do_action('woocommerce_cart_totals_before_shipping'); ?>
+					<?php wc_cart_totals_shipping_html(); ?>
+					<?php do_action('woocommerce_cart_totals_after_shipping'); ?>
+				<?php elseif (WC()->cart->needs_shipping() && 'yes' === get_option('woocommerce_enable_shipping_calc')) : ?>
+					<div class="single-total-content d-flex justify-content-between">
+						<span><?php esc_html_e('Shipping', 'woocommerce'); ?></span>
+						<span class="c-total-price"><?php woocommerce_shipping_calculator(); ?></span>
+					</div>
+				<?php endif; ?>
+
+				<?php
+				if (wc_tax_enabled() && !WC()->cart->display_prices_including_tax()) {
+					$taxable_address = WC()->customer->get_taxable_address();
+					$estimated_text  = '';
+
+					if (WC()->customer->is_customer_outside_base() && !WC()->customer->has_calculated_shipping()) {
+						/* translators: %s location. */
+						$estimated_text = sprintf(' <small>' . esc_html__('(estimated for %s)', 'woocommerce') . '</small>', WC()->countries->estimated_for_prefix($taxable_address[0]) . WC()->countries->countries[$taxable_address[0]]);
+					}
+
+					if ('itemized' === get_option('woocommerce_tax_total_display')) {
+						foreach (WC()->cart->get_tax_totals() as $code => $tax) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+				?>
+							<div class="single-total-content d-flex justify-content-between">
+								<span><?php echo esc_html($tax->label) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+										?></span>
+								<span data-title="<?php echo esc_attr($tax->label); ?>"><?php echo wp_kses_post($tax->formatted_amount); ?></span>
+							</div>
+						<?php
+						}
+					} else {
+						?>
+						<div class="single-total-content d-flex justify-content-between">
+							<span><?php echo esc_html(WC()->countries->tax_or_vat()) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+									?></span>
+							<span class="c-total-price"><?php wc_cart_totals_taxes_total_html(); ?></span>
+						</div>
+				<?php
+					}
+				}
+				?>
+
 				<?php do_action('woocommerce_cart_totals_before_order_total'); ?>
+
+				<hr>
 				<div class="single-total-content d-flex justify-content-between">
 					<span><?php esc_html_e('Total', 'woocommerce'); ?></span>
 					<span class="c-total-price"><?php wc_cart_totals_order_total_html(); ?></span>
 				</div>
+
 				<?php do_action('woocommerce_cart_totals_after_order_total'); ?>
 				<?php do_action('woocommerce_proceed_to_checkout'); ?>
 				<?php do_action('woocommerce_after_cart_totals'); ?>
